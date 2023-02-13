8557689b975d6e02a9070a65afb02495d3333a2c
Barry Van Tassell <bajavata@gmail.com>
2023-02-12 21:58:10 -0800

    add some unit tests

--- src/main.rs
+++ src/main.rs
@@ -199,38 +199,119 @@ fn execute_outcomes(answer: &Answer) -> Vec<OutcomeResult> {
     output
 }
 
+fn create_pinboard_queries(posts: &Vec<pbin::PinboardPost>) -> Vec<Query> {
+    let mut queries = Vec::new();
+
+
+    for (i,post) in posts.iter().enumerate() {
+        let mut answers = Vec::new();
+        answers.push(make_answer(1, "add tags".to_string()));
+        answers.push(make_answer(2, "skip".to_string()));
+        answers.push(make_answer(3, "edit description".to_string()));
+
+        let query: Query = Query {
+            id: i as u16,
+            text: prepare_question_text(&post),
+            answers: answers,
+        };
+        queries.push(query);
+    };
+
+    queries
+}
+
 fn main() {
     let auth: String = env::var("PINBOARD_API_TOKEN").unwrap();
     let p = pbin::PinboardClient::new(auth);
     //println!("{:?}", p.get_all_posts(vec!(("tag".to_string(), "collapse".to_string()))));
-    let last = p.get_posts_recent(1).unwrap();
-    let query: Query = Query {
-        id: 1,
-        text: prepare_question_text(&last.posts[0]),
-        answers: &{
-            let mut v: Vec<Answer> = Vec::new();
-            v.push(make_answer(1, "add tags".to_string()));
-            v.push(make_answer(2, "skip".to_string()));
-            v.push(make_answer(3, "edit description".to_string()));
-            v
-        },
-    };
+    let last = p.get_posts_recent(5).unwrap();
 
-    let chosen = do_query(&query);
-    let from_outcomes = match chosen {
-        Some(ans) => {
-            execute_outcomes(ans)
-        },
-        None => {
-            println!("{:?}", chosen);
-            Vec::new()
-        },
-    };
+    let queries = create_pinboard_queries(&last.posts);
+    println!("{:?}", queries);
 
-    println!("{:?}", from_outcomes);
+    for query in queries {
+        let chosen = do_query(&query);
+
+        println!("{:?}", chosen);
+        
+        let from_outcomes = match chosen {
+            Some(ans) => {
+                execute_outcomes(ans)
+            },
+            None => {
+                println!("{:?}", chosen);
+                Vec::new()
+            },
+        };
+        println!("{:?}", from_outcomes);
+    };
 
 }
 
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    struct SuccessOutcome {}
+    struct FailureOutcome {}
+    struct NextQueryOutcome {}
+
+    impl Outcome for SuccessOutcome {
+        fn handler(&self, display: &str) -> OutcomeResult {
+            OutcomeResult::Success
+        }
+    }
+
+    impl Outcome for FailureOutcome {
+        fn handler(&self, display: &str) -> OutcomeResult {
+            OutcomeResult::Failure
+        }
+    }
+
+    impl Outcome for NextQueryOutcome {
+        fn handler(&self, display: &str) -> OutcomeResult {
+            OutcomeResult::NextQuery(2)
+        }
+    }
+
+    fn fake_pin() -> pbin::PinboardPost {
+        pbin::PinboardPost {
+            href: "http://butts.poop".to_string(),
+            description: "this is the description".to_string(),
+            extended: "this is the extended".to_string(),
+            hash: "abcde123456789".to_string(),
+            time: "this is the timestamp".to_string(),
+            others: "".to_string(),
+            tag: vec!["tag1".to_string(), "tag2".to_string()],
+        }
+    }
+
+    fn fake_answer() -> Answer {
+        let outcomes:Vec<Box<dyn Outcome>> = vec![
+            Box::new(SuccessOutcome {}),
+            Box::new(FailureOutcome {}),
+            Box::new(NextQueryOutcome {}),
+        ];
+        Answer {
+            id: 1,
+            outcomes: outcomes,
+            display: "answer happened".to_string(),
+        }
+    }
+
+    #[test]
+    fn test_execute_outcomes() -> () {
+        let ans = fake_answer();
+        let out = execute_outcomes(&ans);
+        let right = vec![
+            OutcomeResult::Success,
+            OutcomeResult::Failure,
+            OutcomeResult::NextQuery(2),
+        ];
+        assert_eq!(out, right);
+    }
+}
 
 
 
